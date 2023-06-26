5c63638faef2ee32fbcef0f0c1b10bf52762e335
FabrizCastro <fabrizziocastro2612@gmail.com>
2023-06-26 18:15:32 -0500

    add algoritms and interfaz

--- TRABAJOFINAL/Algoritmos_Busqueda_Recorrido.py
+++ TRABAJOFINAL/Algoritmos_Busqueda_Recorrido.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from Grafo import *
 from Nodos import nodes, connections
+from itertools import cycle
 
 # Realizar el recorrido en anchura (BFS)
 def realizar_recorrido_bfs(source):
@@ -21,3 +22,40 @@ def calcular_camino_mas_corto(source, target):
     shortest_path = nx.dijkstra_path(graph, source=source, target=target)
     print("\nCamino más eficaz:")
     print(shortest_path)
+
+def realizar_ordenamiento_topologico():
+    # Realizar el ordenamiento topológico
+    topological_order = list(nx.topological_sort(graph))
+
+    # Ordenar los nodos manualmente basándonos en el dato 'area_local' de cada nodo
+    topological_order.sort(key=lambda node_name: next((node.area_global for node in nodes if node.port_name == node_name), ''))
+
+    # Crear un subgrafo con los nodos en el orden topológico
+    ordered_graph = graph.subgraph(topological_order)
+
+    # Obtener la posición de los nodos en el grafo utilizando el algoritmo 'spring_layout'
+    pos = nx.spring_layout(ordered_graph, seed=42)
+
+    # Crear una paleta de colores para las áreas globales
+    color_palette = {
+        'América del Sur': 'blue',
+        'América del Norte': 'red',
+        'Europa': 'green',
+        'América Central': 'orange',
+        'Asia': 'purple',
+        'Oceanía': 'yellow',
+        'África': 'brown'
+    }
+
+    # Obtener el área global de cada nodo y asignarle un color correspondiente
+    node_colors = [color_palette[next((node.area_global for node in nodes if node.port_name == node_name), '')] for node_name in ordered_graph.nodes()]
+
+    # Dibujar los nodos y las aristas
+    nx.draw_networkx_nodes(ordered_graph, pos, node_size=500, node_color=node_colors)
+    nx.draw_networkx_labels(ordered_graph, pos, font_size=10)
+    nx.draw_networkx_edges(ordered_graph, pos, alpha=0.5)
+
+    # Mostrar el grafo
+    plt.show()
+
+    return ordered_graph
--- TRABAJOFINAL/Interfaz.py
+++ TRABAJOFINAL/Interfaz.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from Algoritmos_Busqueda_Recorrido import *
 from tkinter import messagebox
 
+
 class InterfazGrafica(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -10,51 +11,64 @@ class InterfazGrafica(tk.Tk):
         
         # Agregar imagen de fondo
         self.configure(background='blue')
-        self.geometry("700x700")
+        self.geometry("1000x700")
         
         # Crear los elementos de la interfaz
-        self.label = tk.Label(self, text="Ingrese el nombre del nodo inicial:", bg="#F5F5F5", fg="#333333", font=("Arial", 16))
+        self.label = tk.Label(self, text="Bienvenido a la representación de la interconexión de los grandes puertos del mundo", bg="#F5F5F5", fg="#333333", font=("Berlin Sans FB", 16))
         self.label.pack(pady=10)
         
-        self.entry = tk.Entry(self, font=("Arial", 14))
-        self.entry.pack(pady=10)
-        
-        self.button_bfs = tk.Button(self, text="Recorrido en anchura (BFS)", command=self.mostrar_recorrido_bfs, bg="#007BFF", fg="#FFFFFF", font=("Arial", 14))
-        self.button_bfs.pack(pady=10)
-        
-        self.button_dfs = tk.Button(self, text="Recorrido en profundidad (DFS)", command=self.mostrar_recorrido_dfs, bg="#FFC107", fg="#333333", font=("Arial", 14))
-        self.button_dfs.pack(pady=10)
-        
+        self.button_mostrar_grafo = tk.Button(self, text="Mostrar Grafo", command=self.mostrar_grafo, bg="#DC3545", fg="#FFFFFF", font=("Berlin Sans FB", 14))
+        self.button_mostrar_grafo.pack(pady=(40,20))
+
         self.frame_destino = tk.Frame(self, bg="#F5F5F5")
-        self.frame_destino.pack(pady=(80, 10), padx=10, anchor="w")  # Padding de 80 en la parte superior y 10 en la inferior
+        self.frame_destino.pack(pady=(30, 50))
         
-        self.label_destino = tk.Label(self.frame_destino, text="Seleccione el puerto de destino:", bg="#F5F5F5", fg="#333333", font=("Arial", 16))
+        self.label_destino = tk.Label(self.frame_destino, text="Seleccione el puerto de destino:", bg="#F5F5F5", fg="#333333", font=("Berlin Sans FB", 16))
         self.label_destino.grid(row=0, column=0, padx=10)
 
         self.dropdown_value = tk.StringVar(self)
         self.dropdown_value.set(nodes[0].port_name)  # Establecer el valor inicial del menú desplegable
-        self.dropdown = tk.OptionMenu(self.frame_destino, self.dropdown_value, * [node.port_name for node in nodes])
+        self.dropdown = tk.OptionMenu(self.frame_destino, self.dropdown_value, *[node.port_name for node in nodes])
         self.dropdown.config(width=20)
         self.dropdown.grid(row=0, column=1, padx=10)
         
-        self.label_input = tk.Label(self.frame_destino, text="Destino final:", bg="#F5F5F5", fg="#333333", font=("Arial", 16))
-        self.label_input.grid(row=1, column=0, padx=10, pady=10)
+        self.entry_firstinput = tk.Label(self.frame_destino, text="Inicio:", bg="#F5F5F5", fg="#333333", font=("Berlin Sans FB", 16))
+        self.entry_firstinput.grid(row=1, column=0, padx=10, pady=10)
         
-        self.entry_destino = tk.Entry(self.frame_destino, font=("Arial", 14))
-        self.entry_destino.grid(row=1, column=1, padx=10, pady=10)
+        self.entry = tk.Entry(self.frame_destino, font=("Berlin Sans FB", 14))
+        self.entry.grid(row=1, column=1, padx=10, pady=10)
+
+        self.label_input = tk.Label(self.frame_destino, text="Destino final:", bg="#F5F5F5", fg="#333333", font=("Berlin Sans FB", 16))
+        self.label_input.grid(row=2, column=0, padx=10, pady=10)
         
-        self.button_shortest = tk.Button(self, text="Mostrar camino más eficaz", command=self.mostrar_camino_mas_corto, bg="#28A745", fg="#FFFFFF", font=("Arial", 14))
-        self.button_shortest.pack(pady=10)
+        self.entry_destino = tk.Entry(self.frame_destino, font=("Berlin Sans FB", 14))
+        self.entry_destino.grid(row=2, column=1, padx=10, pady=10)
         
-        self.button_mostrar_grafo = tk.Button(self, text="Mostrar Grafo", command=self.mostrar_grafo, bg="#DC3545", fg="#FFFFFF", font=("Arial", 14))
-        self.button_mostrar_grafo.pack(pady=(100,20))
+        self.label_inputnotentry = tk.Label(self.frame_destino, text="Destino no deseado:", bg="#F5F5F5", fg="#333333", font=("Berlin Sans FB", 16))
+        self.label_inputnotentry.grid(row=3, column=0, padx=10, pady=10)
+        
+        self.not_entry = tk.Entry(self.frame_destino, font=("Berlin Sans FB", 14))
+        self.not_entry.grid(row=3, column=1, padx=10, pady=10)
+
+        self.frame_funciones = tk.Frame(self, bg="#F5F5F5")
+        self.frame_funciones.pack(pady=(10, 0))
+
+        self.button_bfs = tk.Button(self.frame_funciones, text="Recorrido en anchura (BFS)", command=self.mostrar_recorrido_bfs, bg="#007BFF", fg="#FFFFFF", font=("Berlin Sans FB", 14))
+        self.button_bfs.grid(row=0, column=0, pady=10)
+        
+        self.button_dfs = tk.Button(self.frame_funciones, text="Recorrido en profundidad (DFS)", command=self.mostrar_recorrido_dfs, bg="#FFC107", fg="#333333", font=("Berlin Sans FB", 14))
+        self.button_dfs.grid(row=0, column=1, pady=10)
+        
+        self.button_shortest = tk.Button(self.frame_funciones, text="Mostrar camino más eficaz", command=self.mostrar_camino_mas_corto, bg="#28A745", fg="#FFFFFF", font=("Berlin Sans FB", 14))
+        self.button_shortest.grid(row=0, column=2, pady=10)
 
         self.nodo_destino = nodes[0].port_name
+        
 
     def mostrar_recorrido_bfs(self):
         nodo_inicial = self.entry.get()
         realizar_recorrido_bfs(nodo_inicial)
-    
+        
     def mostrar_recorrido_dfs(self):
         nodo_inicial = self.entry.get()
         realizar_recorrido_dfs(nodo_inicial)
@@ -69,9 +83,8 @@ class InterfazGrafica(tk.Tk):
         self.nodo_destino = selected_node
 
     def mostrar_grafo(self):
-        plt.show()
-
-
+        realizar_ordenamiento_topologico()
+        # Realizar alguna acción con el grafo ordenado
 
 if __name__ == "__main__":
     # Crear una instancia del grafo
